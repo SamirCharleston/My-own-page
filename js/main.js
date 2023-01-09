3e3febe9be0630f0c4015521c19e1b66e0c24aca
SamirCharleston <samircharleston@gmail.com>
2023-01-09 17:22:01 -0300

    animação do menu implementada

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const menu = document.querySelector(".menu-bottom");
+const menuHidden = document.querySelector("header .menu-bottom img");
 const buttonHiddenMenu = document.getElementById("hidden-menu");
 
+
 buttonHiddenMenu.addEventListener("click", () => {
-    menu.style.display = "flex";
+
+    menu.style.height = "31px";
+    menu.style.fontSize = "1em";
+    menuHidden.style.height = "unset";
+    buttonHiddenMenu.style.display = "none";
 });
+
+/*When the page is scrolled, the last three options disappear*/
+window.onscroll = () => {
+    menu.style.height = "0";
+    menu.style.fontSize = "0";
+    menuHidden.style.height = "0";
+    buttonHiddenMenu.style.display = "unset";
+};
