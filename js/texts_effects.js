edcb07c4d48c5acfe9461643cb370a300a656d2b
SamirCharleston <samircharleston@gmail.com>
2023-01-11 23:06:19 -0300

    salvando

--- js/texts_effects.js
+++ js/texts_effects.js
@@ -1,23 +1,37 @@
 const titlePresentation = document.getElementById("title-presentation");
 
-typeWriterEffect();
+let wordSequence = (titlePresentations) => new Promise((resolve, reject) => {
+
+    resolve(titlePresentations => {
+        let phrase = titlePresentations.innerHTML.split("-");
+
+        console.log("Cheguei na parte de cima");
+    });
+})
+
+wordSequence(titlePresentation).then(() => {
+    console.log("cheguei na parte de baixo");
+}).catch(() => {
+    console.log("cheguei no Catch");
+})
 
 function typeWriterEffect() {
     let letters = titlePresentation.innerHTML.split('');
+    let totalPhrases = 0;
+
     titlePresentation.innerHTML = "";
 
-    letters.forEach((letter, i) => {
-
-        let phrase += letter;
-
-        if (letter !== "-") {
-            setTimeout(() => {
-                titlePresentation.innerHTML += letter;
-            }, 80 * i);
-        } else {
-            setTimeout(() => {
-                titlePresentation.innerHTML += "<br>";
-            }, 80 * i);
+    for (i in letters) {
+        if (letters[i] === '-') {
+            totalPhrases++;
         }
-    })
+    }
+}
+
+function typeWrite(word, callback) {
+
+}
+
+function typeErase(word, callback) {
+
 }
